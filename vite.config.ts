08dba2ff94e4be3ceb923ba7e1aad735e3b515dd
Paul Boes <pvboes@gmail.com>
2026-03-18 16:58:32 +0100

    Add LOCAL_DEV and baseUrl to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,34 +2,37 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { defineConfig } from 'vite';
 
+const isLocal = process.env.LOCAL_DEV === 'true';
 const baseUrl = process.env.NODE_ENV !== 'production' ? 'circles-dev.gnosis.io' : 'circles.gnosis.io';
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    nodePolyfills({
-      include: ['buffer', 'process', 'util', 'stream', 'events'],
-      globals: { Buffer: true, global: true, process: true }
-    })
-  ],
-  server: {
-	host: baseUrl,
-	port: 443,
-	https: {
-		key: `./${baseUrl}-key.pem`,
-		cert: `./${baseUrl}.pem`
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			include: ['buffer', 'process', 'util', 'stream', 'events'],
+			globals: { Buffer: true, global: true, process: true }
+		})
+	],
+	server: isLocal
+		? { host: 'localhost', port: 5173 }
+		: {
+			host: baseUrl,
+			port: 443,
+			https: {
+				key: `./${baseUrl}-key.pem`,
+				cert: `./${baseUrl}.pem`
+			}
+		},
+	optimizeDeps: {
+		esbuildOptions: {
+			define: { global: 'globalThis' }
+		}
+	},
+	resolve: {
+		alias: {
+			process: 'process/browser',
+			buffer: 'buffer',
+			util: 'util'
+		}
 	}
-},
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: 'globalThis' }
-    }
-  },
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      buffer: 'buffer',
-      util: 'util'
-    }
-  }
 });
