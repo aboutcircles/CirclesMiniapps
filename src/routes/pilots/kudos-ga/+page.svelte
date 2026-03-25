0cde3ebcc8b992ae441c2969330e654404cb068f
Paul Boes <pvboes@gmail.com>
2026-03-25 10:06:02 +0100

    fix: only clear message on top row click, not on input or emoji chip interactions

--- src/routes/pilots/kudos-ga/+page.svelte
+++ src/routes/pilots/kudos-ga/+page.svelte
@@ -274,9 +274,8 @@
 					href="https://app.gnosis.io/transfer/{ORG_ADDRESS}/crc/1?data={encodeKudosData(recipientAddress, kudosMessage)}"
 					target="_blank"
 					rel="noopener noreferrer"
-					onclick={() => { setTimeout(() => { kudosMessage = ''; }, 1000); }}
 				>
-					<div class="kudos-top-row">
+					<div class="kudos-top-row" onclick={() => { setTimeout(() => { kudosMessage = ''; }, 1000); }}>
 						<span class="kudos-arrow">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="22" height="22">
 								<path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
